645c31662d000f879515ec9ed430f905ceff20ac
Nicolas Penin <nicolas.penin@dragon-angel.fr>
2018-06-21 15:57:20 +0200

    fixed config access

--- src/server/index.ts
+++ src/server/index.ts
@@ -4,16 +4,13 @@ import { scrapper } from '@domojs/media';
 import { DbTvShow, tvdbScrapper, setLanguage } from './scrapper';
 export * from './scrapper';
 
-akala.injectWithNameAsync(['$isModule', '$config.@domojs/media-tmdbscrapper', '$agent.media'], function (isModule: akala.worker.IsModule, config: PromiseLike<any>, client: Client<Connection>)
+akala.injectWithNameAsync(['$isModule', '$config.@domojs/media-tmdbscrapper', '$agent.media'], function (isModule: akala.worker.IsModule, config: any, client: Client<Connection>)
 {
     if (isModule('@domojs/media-tmdbscrapper'))
     {
         if (config)
-            config.then(function (config)
-            {
-                if (config && config.language)
-                    setLanguage(config.language);
-            });
+            if (config && config.language)
+                setLanguage(config.language);
 
         var s = akala.api.jsonrpcws(scrapper).createClient(client)({
             scrap: function (media: DbTvShow)
